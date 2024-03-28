1560963e073c033fa8cf4e6b5e699f7aca2259b7
PeterKrall <80395903+PeterKrall@users.noreply.github.com>
2024-03-28 12:34:45 +0100

    solution implemented and tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+library(matlib)
+## Functions creating an object wrapping a matrix and providing 
+## access to the inverse, using cache
 
-## Write a short comment describing this function
+## wrap the matrix provided as argument in an object that provides
+## access to the inverse, using superassignment for caching the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    }
 
-}
 
-
-## Write a short comment describing this function
+## retrieve the cached inverse of the matrix wrapped in an 
+## object constructed by makeCacheMatrix, compute and cache if
+## not done previously
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(is.null(m)) {
+      message("caching inverse of wrapped matrix")
+      data <- x$get()
+      m <- inv(data, ...)
+      x$setinv(m)
+    } else {
+      message("using cached inverse of wrapped matrix")
+    }
+    m  
 }
